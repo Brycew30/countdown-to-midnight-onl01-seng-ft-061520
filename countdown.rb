e8723d9a7220199142353cd8e0fa8afb66dabf56
Bryce Whittington <brycew30@gmail.com>
2020-06-16 17:41:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,10 +10,10 @@ end
 countdown(10)
 
 def countdown_with_sleep(number)
- while number >0
- sleep (1)
+ while number > 0
   puts "#{number} SECOND(S)!"
   number -= 1
- return "HAPPY NEW YEAR!"
+    sleep 1
  end
+ return "HAPPY NEW YEAR!"
 end
